a7db8a1ab82cf22b3655465e1b1b38c1eb92f8b2
garcianavalon <garcianavalon@gmail.com>
2015-05-08 11:51:15 +0200

    multiple regions

--- openstack_dashboard/dashboards/idm_admin/user_accounts/forms.py
+++ openstack_dashboard/dashboards/idm_admin/user_accounts/forms.py
@@ -42,7 +42,7 @@ class UserAccountsLogicMixin():
                 use_idm_account=self.use_idm_account))
         return trial_users >= getattr(settings, 'MAX_TRIAL_USERS', 0)
 
-    def update_account(self, request, user_id, role_id, region_id=None):
+    def update_account(self, request, user_id, role_id, regions):
         activate_cloud = role_id != fiware_api.keystone.get_basic_role(
             request, use_idm_account=self.use_idm_account).id
         
@@ -74,25 +74,24 @@ class UserAccountsLogicMixin():
         else:
             self._deactivate_cloud(request, user_id, user.cloud_project_id)
         
-        # assign endpoint group for the selected region
-        if not region_id:
-            return
+        # assign endpoint group for the selected regions
+        for region in regions:
+            endpoint_groups = fiware_api.keystone.endpoint_group_list(
+                request, use_idm_account=self.use_idm_account)
+            region_group = next(group for group in endpoint_groups
+                if group.filters.get('region_id', None) == region)
 
-        endpoint_groups = fiware_api.keystone.endpoint_group_list(
-            request, use_idm_account=self.use_idm_account)
-        region_group = next(group for group in endpoint_groups
-            if group.filters.get('region_id', None) == region_id)
-
-        if not region_group:
-            messages.error(
-                request, 'There is no endpoint group defined for that region')
-            return
+            if not region_group:
+                messages.error(
+                    request,
+                    'There is no endpoint group defined for {}'.format(region))
+                continue
         
-        fiware_api.keystone.add_endpoint_group_to_project(
-            request,
-            project=user.cloud_project_id,
-            endpoint_group=region_group,
-            use_idm_account=self.use_idm_account)
+            fiware_api.keystone.add_endpoint_group_to_project(
+                request,
+                project=user.cloud_project_id,
+                endpoint_group=region_group,
+                use_idm_account=self.use_idm_account)
 
         # done!
 
@@ -207,7 +206,6 @@ def get_account_choices():
 def get_regions():
     """Loads all posible regions for the FIWARE cloud portal"""
     choices = [
-        ('', 'No region'),
     ]
     regions = fiware_api.keystone.region_list(None, use_idm_account=True)
     for region in regions:
@@ -223,7 +221,7 @@ class UpdateAccountForm(forms.SelfHandlingForm, UserAccountsLogicMixin):
         label=("Account type"),
         choices=get_account_choices())
 
-    region = forms.ChoiceField(
+    regions = forms.MultipleChoiceField(
         required=False,
         label=("Select new Cloud region"),
         choices=get_regions())
@@ -244,6 +242,11 @@ class UpdateAccountForm(forms.SelfHandlingForm, UserAccountsLogicMixin):
         
         return account_type
 
+    def clean_regions(self):
+        """Make sure we get an emtpy list"""
+        regions = self.cleaned_data.get('regions', [])
+        return regions
+
     def clean(self):
         cleaned_data = super(UpdateAccountForm, self).clean()
         account_type = cleaned_data.get('account_type', None)
@@ -254,7 +257,7 @@ class UpdateAccountForm(forms.SelfHandlingForm, UserAccountsLogicMixin):
         role_name = next(choice[1] for choice in get_account_choices()
             if choice[0] == account_type)
         
-        region = cleaned_data.get('region', None)
+        regions = cleaned_data.get('regions', [])
 
         allowed_regions = getattr(settings, 'FIWARE_ALLOWED_REGIONS', None)
         if not allowed_regions:
@@ -263,21 +266,22 @@ class UpdateAccountForm(forms.SelfHandlingForm, UserAccountsLogicMixin):
                 code='invalid')
         
         if not allowed_regions[role_name]:
-            if region:
+            if regions:
                 messages.info(self.request, 
                     'The account type selected is not allowed any region')
 
-            cleaned_data['region'] = None
+            cleaned_data['regions'] = []
             return cleaned_data
 
-        if not region in allowed_regions[role_name]:
-            if not region:
-                msg = 'You must choose a region for this accout type.'
-            else:
-                msg = 'The region {0} is not allowed for that account type.'.format(region)
-            raise forms.ValidationError(
-                msg,
-                code='invalid')
+        for region in regions:
+            if not region in allowed_regions[role_name]:
+                if not region:
+                    msg = 'You must choose a region for this accout type.'
+                else:
+                    msg = '{0} is not allowed for that account type.'.format(region)
+                raise forms.ValidationError(
+                    msg,
+                    code='invalid')
 
         return cleaned_data
 
@@ -286,9 +290,9 @@ class UpdateAccountForm(forms.SelfHandlingForm, UserAccountsLogicMixin):
         try:
             user_id = data['user_id']
             role_id = data['account_type']
-            region_id = data['region']
+            regions = data['regions']
 
-            self.update_account(request, user_id, role_id, region_id)
+            self.update_account(request, user_id, role_id, regions)
 
             messages.success(request,
                 'User account upgraded succesfully')
