0a5d4a96c4f656ff2aa154274dc19cfa3f4d3d17
FedericoFdez <fefernandez@dit.upm.es>
2016-09-27 11:08:27 +0200

    Improve usability of contextual-help popovers

--- openstack_dashboard/static/fiware/contextualHelp.js
+++ openstack_dashboard/static/fiware/contextualHelp.js
@@ -4,7 +4,7 @@ $( document ).ready(function () {
 	});
 
 	$('body').click(function (e) {
-		$('[data-toggle="popover"]').each(function () {
+		$('[data-toggle="popover"].contextual-help').each(function () {
 	        //the 'is' for buttons that trigger popups
 	        //the 'has' for icons within a button that triggers a popup
 	        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
